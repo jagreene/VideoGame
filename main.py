2d26bf655e664842e9f154eab0827a2dbf653293
jagreene <austin.greene@students.olin.edu>
2014-03-06 13:07:07 -0500

    Fixed collision detection game is fun now

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Model:
         self.screen = screen
         self.map = {}
         self.blocks = Group()
-        self.player = Player(25,850)
+        self.player = Player(25,825)
         self.players = Group()
         self.players.add(self.player)
 
@@ -22,7 +22,7 @@ class Model:
             for columns in xrange(0,(size[0]/50)):
                 if rows == (size[1]/50)-1 or rows == 0:
                     map.update({(rows,columns):"block"})
-                elif(rows%3 == 0):
+                elif(rows%5 == 0):
                     map.update({(rows,columns):random.choice(map_options)})
                 else:
                     map.update({(rows,columns):"empty"})
@@ -36,29 +36,30 @@ class Model:
 
     def draw(self):
         self.screen.fill(blackColor)
-        self.blocks.update()
         self.blocks.draw(self.screen)
-        self.players.update()
         self.players.draw(self.screen)
 
+    def update(self):
+        self.blocks.update()
+        self.players.update()
+
     def checkPlayerBlockCollisions(self,players,blocks):
-        print "Checking Collision"
-        self.onGround         = False
-        self.hittingWallRight = False
-        self.hittingCeilling  = False
 
-        print self.onGround
-
+        print self.player.onGround
         for collision in spritecollide(self.player, self.blocks, False):
             print "COLISION!!!"
-            if abs(self.player.rect.bottom - collision.rect.centery) < 25 and abs(self.player.lastY - collision.rect.centery)>25:
+
+            if 22 <= abs(self.player.rect.bottom - collision.rect.centery) <= 25:
                 self.player.onGround = True
-            elif self.player.rect.top - 1 < collision.rect.centery < 25 and self.player.lastY + 14 - collision.rect.centery > 25:
+            if abs(self.player.rect.bottom - collision.rect.centery) < 25:
+                self.player.onGround = True
+            elif self.player.rect.top - collision.rect.centery < 25:
                 self.player.hittingCeilling = True
-            elif abs(self.player.rect.right - collision.rect.centerx) < 25:
+            if abs(self.player.rect.right - collision.rect.centerx) < 27 and abs(self.player.rect.centery - collision.rect.centery) < 30:
                 self.player.hittingWallRight = True
-            elif self.player.rect.left - 1 < collision.rect.y < self.player.rect.left + 1:
+            if abs(self.player.rect.left - collision.rect.centerx) < 27 and abs(self.player.rect.centery - collision.rect.centery) < 30:
                 self.player.hittingWallLeft = True
+        print self.player.onGround
 
 
 class Block(Sprite):
@@ -81,51 +82,67 @@ class Player(Sprite):
         self.rect  = self.image.get_rect()
         self.mask  = pygame.mask.from_surface(self.image)
 
-        self.lastX = x
-        self.lastY = y
-        self.x     =  x
-        self.y     =  y
-        self.vx    =  0
-        self.vy    =  0
-        self.fx    =  0
-        self.fy    =  0
-        self.mass  =  5
-        self.fg    =  0
-        self.ff    =  0
-        self.fn    =  0
-        self.mu    = .1
-    
-        self.rect.bottomleft=(self.x,self.y)
 
-        self.onGround         = True
+        self.x      =  x
+        self.y      =  y
+        self.vx     =  0
+        self.vy     =  0
+        self.fx     =  0
+        self.fy     =  0
+        self.lastX  =  x
+        self.lastY  =  y
+        self.lastVx =  0
+        self.lastVy =  0
+        self.lastFx =  0
+        self.lastFy =  0
+        self.mass   =  5
+        self.fg     =  0
+        self.ff     =  0
+        self.fn     =  0
+        self.mu     = .6
+    
+        self.rect.center=(self.x,self.y)
+
+        self.onGround         = False
+        self.inGround         = False
         self.hittingCeilling  = False
         self.hittingWallRight = False
         self.hittingWallLeft  = False
 
+        self.jumpCount = 0
+        self.terminalVelocity = 3
+
 
     def moveForward(self):
-        self.vx = 5 
+        self.vx = 3
     def moveBackward(self):
-        self.vx = -5
+        self.vx = -3
     def jump(self):
-        self.vy = -5
+        self.vy = -8
     def setFriction(self):
         if int(self.vx) != 0:
             self.ff = -abs(self.vx)/self.vx*self.mu*abs(self.fn)
         else:
             self.ff = 0
     def setGravity(self):
-        self.fg =  2*self.mass
+        self.fg =  .25*self.mass
     def setNormalForce(self):
         if self.onGround:
-            self.fn = -(self.vy*1)*self.mass-self.fg
+            self.fn = -self.fg
+            self.vy = 0
+        if self.inGround:
+            self.fn = -self.fg
+            self.vy = -5
         if self.hittingCeilling:
-            self.fn =  (self.vy*1)*self.mass+self.fg
+            self.fn = 1
+            self.vy = 2
         if self.hittingWallRight:
-            self.fn = -(self.vx*1)*self.mass
+            self.fn = -1
+            self.vx = -3
         if self.hittingWallLeft:
-            self.fn =  (self.vx*1)*self.mass
-        elif (not self.onGround) and (not self.hittingCeilling) and (not self.hittingWallLeft) and (not hittingWallRight):
+            self.fn = 1
+            self.vx = 3
+        elif (not self.onGround) and (not self.hittingCeilling) and (not self.hittingWallLeft) and (not self.hittingWallRight):
             self.fn = 0
 
     def update(self):
@@ -139,18 +156,31 @@ class Player(Sprite):
         self.vx += self.fx/self.mass
         self.vy += self.fy/self.mass
 
+        if self.vy > self.terminalVelocity:
+            self.vy = self.terminalVelocity
+
         self.lastX = self.x
         self.lastY = self.y
         self.x += int(self.vx)
         self.y += int(self.vy)
-        print "force = " + str(self.fy)
-        print "velocity = " + str(self.vy)
-        print "Collision Ground "     + str(self.onGround)
-        print "Collision Ceilling "   + str(self.hittingCeilling)
-        print "Collision Wall Left "  + str(self.hittingWallLeft)
-        print "Collision Wall Right " + str(self.hittingWallRight)
 
-        self.rect.bottomleft=((int(self.x)),int(self.y))
+        self.onGround         = False
+        self.hittingCeilling  = False
+        self.hittingWallRight = False
+        self.hittingWallLeft  = False
+        # print "Force in Y is " + str(self.fy)
+        # print "V in Y is " + str(self.vy)
+        # print "Collision Ground "     + str(self.onGround)
+        # print "Collision Ceilling "   + str(self.hittingCeilling)
+        # print "Collision Wall Left "  + str(self.hittingWallLeft)
+        # print "Collision Wall Right " + str(self.hittingWallRight)
+
+        if self.lastVx != 0:
+            if self.vx/self.lastVx < 0:
+                self.image = transform.flip(self.image,True,False)
+                self.rect  = self.image.get_rect()
+
+        self.rect.center = ((int(self.x)),int(self.y))
 
 
 whiteColor = pygame.Color(255,255,255)
@@ -169,6 +199,8 @@ m = Model(screen)
 
 running  = True
 new_room = True
+m.player.setGravity()
+
 while running:
     if new_room:
         m.createMap()
@@ -177,29 +209,52 @@ while running:
 
     keys = pygame.key.get_pressed()
     m.checkPlayerBlockCollisions(m.player, m.blocks)
-    m.player.setGravity()
+
+
+    if keys[pygame.K_ESCAPE]:
+        running = False
     if m.player.onGround:
-        m.player.setFriction()
-        m.player.setNormalForce()
+        if keys[pygame.K_RIGHT]: 
+            m.player.moveForward()
+        elif keys[pygame.K_LEFT]:
+            m.player.moveBackward()                
+
+    m.player.setNormalForce()
+    print m.player.onGround
+    m.player.setFriction()
 
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
         if event.type == KEYDOWN:
-            if event.key == pygame.K_UP:
+            if event.key == pygame.K_UP and m.player.jumpCount < 2:
                 m.player.jump()
+                m.player.jumpCount +=1
+            if (m.player.hittingWallRight or m.player.hittingWallLeft):
+                m.player.jumpCount -= 1
+            elif m.player.onGround:
+                m.player.jumpCount = 0
 
-    if keys[pygame.K_ESCAPE]:
-        running = False
-    if keys[pygame.K_RIGHT]: 
-        m.player.moveForward()
-    elif keys[pygame.K_LEFT]:
-        m.player.moveBackward()
+    m.update()
 
-            #elif event.key == pygame.K_UP:
-                
+    m.player.lastx  = m.player.x
+    m.player.lasty  = m.player.y
+    m.player.lastVx = m.player.vx
+    m.player.lastVy = m.player.vy
+    m.player.lastFx = m.player.fx
+    m.player.lastFy = m.player.fy
 
+    m.checkPlayerBlockCollisions(m.player, m.blocks)
+    m.player.setNormalForce()
+    m.player.setFriction()
+    m.update()
+
+    m.player.lastx  = m.player.x
+    m.player.lasty  = m.player.y
+    m.player.lastVx = m.player.vx
+    m.player.lastVy = m.player.vy
+    m.player.lastFx = m.player.fx
+    m.player.lastFy = m.player.fy
 
     m.draw()
     pygame.display.update()
-    time.sleep(1.0/120.0)
